24abc459108378c601e7f29185ab373c020ed431
Anar Chingiskhuu <chingiskhuu.a20@htlwienwest.at>
2023-12-14 14:26:10 +0100

    workbox
    Signed-off-by: Anar Chingiskhuu <chingiskhuu.a20@htlwienwest.at>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,37 @@ export default defineConfig({
   server: {
     port: 8080,
   },
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate", manifest })],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: "src/quasar-variables.sass",
+    }),
+    VitePWA({
+      manifest,
+      includeAssets: ["**/*.{js,css,html,jpg,ico,png,ttf,woff2}"],
+      workbox: {
+        mode: "development",
+        runtimeCaching: [
+          {
+            urlPattern: "/",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "records",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 1, // <== 1 day
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
