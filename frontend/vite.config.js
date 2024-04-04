cab3e4a68298a197a577eaeaa59bc833d045c467
Anarxp <chingiskhuu.a20@htlwienwest.at>
2024-04-04 20:32:44 +0200

    Fix errors, add vueuse

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,37 @@
-import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
-import vue from "@vitejs/plugin-vue";
-import { URL, fileURLToPath } from "url";
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import manifest from "./manifest.js";
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { URL, fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 8080,
     proxy: {
-    "/fotos": "http://localhost:3000/",
+      '/fotos': 'http://localhost:3000/',
+    },
   },
-  },
-  
+
   plugins: [
     vue({
       template: { transformAssetUrls },
     }),
     quasar({
-      sassVariables: "src/quasar-variables.sass",
+      sassVariables: 'src/quasar-variables.sass',
     }),
     VitePWA({
       manifest,
-      includeAssets: ["**/*.{js,css,html,jpg,ico,png,ttf,woff2}"],
+      includeAssets: ['**/*.{js,css,html,jpg,ico,png,ttf,woff2}'],
       workbox: {
-        mode: "development",
+        mode: 'development',
         runtimeCaching: [
           {
-            urlPattern: "/",
-            handler: "NetworkFirst",
+            urlPattern: '/',
+            handler: 'NetworkFirst',
             options: {
-              cacheName: "records",
+              cacheName: 'records',
               expiration: {
                 maxEntries: 50,
                 maxAgeSeconds: 60 * 60 * 24 * 1, // <== 1 day
@@ -47,7 +47,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
